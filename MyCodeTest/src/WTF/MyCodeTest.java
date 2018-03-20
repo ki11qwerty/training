5ab1dbd05e1a956f3bb5069ca0eb030391c5f4f4
ki11qwerty <tasuke34@gmail.com>
2018-03-20 12:13:19 +0300

    20.03

---
+++ MyCodeTest/src/WTF/MyCodeTest.java
@@ -0,0 +1,10 @@
+package WTF;
+public class MyCodeTest {
+    public static void main(String args[]){
+      MyCodeTest mt = new MyCodeTest();
+    }
+
+    public int addCalc(int a, int b, int c){
+        return a+b+c;
+    }
+}
