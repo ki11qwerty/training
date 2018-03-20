91c6c66df753a15566a0f9fe3360628a3d3ded35
ki11qwerty <tasuke34@gmail.com>
2018-03-20 13:31:07 +0300

    20.03

---
+++ MyCodeTest/src/main/java/MyCode/MyCode.java
@@ -0,0 +1,18 @@
+package MyCode;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyCode
+{
+    public static void main( String[] args ) {
+        MyCode mc = new MyCode();
+        System.out.println(mc.addCalc(5,6,7));
+
+
+    }
+    public  int addCalc(int a, int b, int c){
+        return a+b+c;
+    }
+}
