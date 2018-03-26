27e3bfaf53564bd011d0e8832f12f86786caa4ac
ki11qwerty <tasuke34@gmail.com>
2018-03-26 20:00:48 +0300

    26.03

--- MyCode01/src/test/java/ki11qwerty/TestMain.java
+++ MyCode01/src/test/java/ki11qwerty/TestMain.java
@@ -1,33 +1,33 @@
-package ki11qwerty;
+//package ki11qwerty;
+//
+//
+//import org.junit.*;
+//
+//public class TestMain {
+//    Main mn;
+//
+//    @Before
+//    public void startTest() {
+//        mn = new Main();
+//    }
+//
+//    @Test
+//    public void testAdd() {
+//        Assert.assertEquals(4, mn.calcAdd(2, 2));
+//    }
+//
+//    @Test
+//    public void testDeduct() {
+//        Assert.assertEquals(4, mn.calcDeduct(8, 4));
+//    }
+//    @Test
+//    public void testDiv() {
+//        Assert.assertEquals(4, mn.calcDiv(80, 20));
+//    }
+//    @Test
+//    public void testMult() {
+//        Assert.assertEquals(20, mn.calcMult(4, 5));
+//    }
+//    @After
 
-
-import org.junit.*;
-
-public class TestMain {
-    Main mn;
-
-    @Before
-    public void startTest() {
-        mn = new Main();
-    }
-
-    @Test
-    public void testAdd() {
-        Assert.assertEquals(4, mn.calcAdd(2, 2));
-    }
-
-    @Test
-    public void testDeduct() {
-        Assert.assertEquals(4, mn.calcDeduct(8, 4));
-    }
-    @Test
-    public void testDiv() {
-        Assert.assertEquals(4, mn.calcDiv(80, 20));
-    }
-    @Test
-    public void testMult() {
-        Assert.assertEquals(20, mn.calcMult(4, 5));
-    }
-    @After
-
-}
+//}
