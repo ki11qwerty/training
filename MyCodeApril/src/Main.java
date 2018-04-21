14d16eee2f92281a2524f8b89dde618d2d22b79a
ki11qwerty <tasuke34@gmail.com>
2018-04-21 20:41:12 +0300

    21.04

--- MyCodeApril/src/Main.java
+++ MyCodeApril/src/Main.java
@@ -1,12 +1,13 @@
+import java.lang.reflect.Modifier;
+
 public class Main {
 
     public static void main(String[] args) {
 	Notebook nb = new Notebook("Acer r7","i7", "ddr5", 8,
                                "Geforce", 2);
-	Notebook nb1 = new Notebook("wtf");
-        System.out.println(nb.getName()+"-"+nb.getCpu());
-        System.out.println(nb1.getName()+"-"+nb1.getCpu());
-        nb1.setName("emptyBox");
-        System.out.println(nb1.getName());
+	Class inform = nb.getClass();
+        System.out.println(inform);
+        System.out.println(inform.getConstructors());
+        System.out.println(inform.getName());
     }
 }
