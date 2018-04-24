c485a89405bfaaaa9c6f3a5124b20929bb9bc82b
ki11qwerty <tasuke34@gmail.com>
2018-04-24 18:13:40 +0300

    24.04

--- MyCodeApril/src/MyCode1/Main.java
+++ MyCodeApril/src/MyCode1/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Main mn = new Main();
         PrintInfo pi = new PrintInfo();
-	    Notebook nb = new Notebook("Acer r7","i7", "ddr5", 8,
-                               "Geforce", 2);
-	    Notebook nbClear = new Notebook("empty");
-	    Computer cp = new Computer();
-	    pi.printConstructors(nb);
+        Notebook nb = new Notebook("Acer r7", "i7", "ddr5", 8,
+                "Geforce", 2);
+        Notebook nbClear = new Notebook("empty");
+        Computer cp = new Computer();
+        pi.printConstructors(nb);
         pi.printConstructors(cp);
         pi.printConstructors(nbClear);
         pi.printFields(nb);
