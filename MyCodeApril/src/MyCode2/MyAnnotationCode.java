4367d5a647b680ecb81e46510572a4cb2d8ddf0f
ki11qwerty <tasuke34@gmail.com>
2018-04-24 19:23:17 +0300

    24.04

--- MyCodeApril/src/MyCode2/MyAnnotationCode.java
+++ MyCodeApril/src/MyCode2/MyAnnotationCode.java
@@ -1,4 +1,34 @@
 package MyCode2;
 
-public class MyAnnotationCode {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface AdvancedAnnotation {
+    float value() default 5.0f;
 }
+
+class MainClass {
+    @AdvancedAnnotation(value = 20.0f)
+    public void advAnnotatedMethod() {
+        System.out.println("...");
+    }
+
+    public static void main(String[] args) {
+        try {
+            Method m = MainClass.class.getMethod("advAnnotatedMethod", null);
+            AdvancedAnnotation annotation =
+                    m.getAnnotation(AdvancedAnnotation.class);
+            System.out.println("value: " + annotation.value());
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//Результат:
+//        value: 20.0
