24470c684a684b474150a1ef0f150dc801d6f383
Aleksey Antonov <tasuke34@gmail.com>
2019-02-03 18:19:43 +0300

    03.02

--- KotlinTraining/src/Animal.kt
+++
@@ -1,5 +0,0 @@
-abstract class Animals(val name: String,val stamina:Int){
-    abstract fun voice()
-    abstract fun run(distance:Int)
-    abstract fun sleep(time:Int)
-}
--- KotlinTraining/src/Main.kt
+++ KotlinTraining/src/Main.kt
@@ -1,104 +1,24 @@
 import java.util.*
-//import kotlinx.coroutines.*
-
-class Person(val name:String, val age: Int, val someInfo: String)
-
+import kotlin.IllegalArgumentException
 fun main(args : Array<String>) {
-    val person1 = Person("Алексей", 22, "информация отсутствует")
-    val printAllFields: Person.() -> Unit = {
-        println("name: $name\nage: $age\nsomeInfo: $someInfo")
-    }
 
-    person1.printAllFields()
-    val person2 = Person("Александр", 35, "SEO")
-    person2.printAllFields()
-
-    var list: MutableList<String> = mutableListOf()
-    for (x in 1..10) {
-        list.add("some$x")
-    }
-    println("list$list")
-    println("item5 in list: ${list[4]}")
-    val readOnly: List<String> = list
-    println("readOnly = $readOnly")
-
-    var listInt: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(listInt.filter { it % 2 == 0 })
-    println(listInt)
-    println(listInt.filter { it in 5..9 })
-    println(listInt.filter { it in 9 downTo 3 step 3 })
-
-    var myDog = Dog("tuzic", 500)
-    var myDog2 = Dog("sharik", 750)
-    myDog.run(450)
-    myDog2.run(450)
-    myDog.run(1000)
-    myDog2.run(1000)
-    myDog.voice()
-    myDog2.voice()
-
-    val text = """
-         ----------------------------------------------------------------------------------------------------
-                                               some text
-                                         its 44 row in code
-                                  tomorrow again I GO TO MY JOB XD
-                    P.S. - How its Works???? WTF )))))))))))))))))))))))))))))))))))))
-                    .∧＿∧
-                    ( ･ω･｡)つ━☆・*。
-                     ⊂　 ノ 　　　・゜+.
-                     しーＪ　　　°。+ *´¨)
-　　　　　　　　                 　.· ´¸.·*´¨) ¸.·*¨)
-　　　　　　　                       　　　(¸.·´ (¸.·'*
-         ----------------------------------------------------------------------------------------------------
-        """
-    println(text)
-    //TODO: круто, ничего не скажешь... остановился на -> kotlin -> основы -> Операторы перехода
-    // TODO: с такими темпами можно и за пару недель освоить основы я думаю)
-
-    var rand = Random()
-    loop@ for (x in 1..1000) {
-        var randomInt: Int = rand.nextInt(100) + 1
-        if (randomInt == 42) {
-            println("okay its 42")
-            break@loop
-        }
-        when (randomInt) {
-            in 1..29 -> println(">0 this <30")
-            in 30..60 -> println(">=30 this <=60")
-            in 61..100 -> println(">60 this <=100")
-            else -> {
-                println("something Wrong =(")
-            }
-        }
-    }
-    //TODO:классы и наследование
-
-//    //TODO: ну это пиздец какой то, пол дня проебаться с Грэдлом, потом пол дня вот с этой хуйней коррутины блять им. прям бомбанул
-//    fun multi() {
-//        repeat(100_000) {
-//            launch {
-//                delay(1000L)
-//                print(".")
-//            }
-//        }
-//    }
-
-    var myClass = MyClass("name", 28)
-
-    fun MyClass.info():Unit = println("$name,$age") //- это сделано через расширение, прикольная штука однако"
-
-    myClass.info()
-            //TODO: классы, расширения, дата классы и тому подобное, учу доки пока что
-    //TODO: ананимные классы, Анонимные обьекты, обобщения, изолированные классы, перечисления, Модификаторы доступа, все по докам попорядку идет пока что
-
-    //TODO: гикбрейнс видос про андройд разработку на котлин с нуля 1.10 , надо досмотреть
-    //TODO: все около лямбд и функции
-
-    //TODO: доклады джетБрейнс, теория, задачки по котлину на сайте -  https://try.kotlinlang.org/#/Kotlin%20Koans/Introduction/Named%20arguments/Task.kt
-
-
-// лямбды и не только
-
+    randomThis()
+    somethingInt = 1000
+    println("$somethingInt")
 
 }
-data class MyClass(var name:String, var age:Int)
+fun randomThis() {
+        println(somethingInt)
+}
+var somethingInt = try{
+    val random = Random()
+    val someInt= random.nextInt(1000)
+    if(someInt %2 == 0){
+        someInt
+    }else{
+        throw IllegalArgumentException()
+    }
+}catch (e:IllegalArgumentException){
+    println("IllegalArg")
+    -1
+}
