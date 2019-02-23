c2198602f44f0bb7444930f6c762ceb6150d5ab4
Aleksey Antonov <tasuke34@gmail.com>
2019-02-24 00:27:48 +0300

    24.02.19

--- KotlinFebTraining19/src/main/kotlin/Main.kt
+++ KotlinFebTraining19/src/main/kotlin/Main.kt
@@ -1,24 +1,24 @@
 import kotlinx.coroutines.*
 fun main(args: Array<String>) {
-    println("and we go")
+    println("Here we go")
     i = 0
     breakingFun()
     for(x in 0..10) {
-        i =+ x
-        println(i)
+        println("for in main $x")
     }
-    println("дайте шума")
+    getBoolean()
+    println("end main")
 }
 suspend fun someFun(){
-   println("someFun")
+   println("start someFun")
     for (x in 0..10){
-        println("$x")
+        println("$x in someFun")
         delay(10)
     }
     print("-end someFun-")
 }
 fun breakingFun() = runBlocking {
-    println("breakingFun")
+    println("starting breakingFun")
     val jobs = List(10){
         launch{
             delay(1)
@@ -29,14 +29,33 @@ fun breakingFun() = runBlocking {
     }
     println("--------------------------")
     //jobs.forEach{ it.join()}
-    val myJobs2 = List(20){
+    println("starting myJobs2")
+    val myJobs2 = List(20) {
         launch {
             delay(10)
-            println("\n \n йебой")
+            print(" $it in jobs2")
         }
     }
-    myJobs2.forEach{it.join()}
+    myJobs2.forEach { it.join() }
 }
+suspend fun returnTrue(): Boolean {
+    delay(1000)
+    return true
+}
+suspend fun returnFalse(): Boolean{
+    delay(1000)
+    return false
+}
+fun getBoolean() = runBlocking {
+    val jobs3 = List(3) {
+        launch {
+            println("${returnFalse()} ${returnTrue()}")
+        }
+    }
+    println("in end of getBoolean()")
+}
+
+
 
 var i = 0
 
