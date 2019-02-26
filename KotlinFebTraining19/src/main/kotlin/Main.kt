d5f792b63d6dfb9ab9c6da843eb95571022ed99e
Aleksey Antonov <tasuke34@gmail.com>
2019-02-26 14:06:52 +0300

    26.02.19

--- KotlinFebTraining19/src/main/kotlin/Main.kt
+++ KotlinFebTraining19/src/main/kotlin/Main.kt
@@ -1,60 +1,34 @@
 import kotlinx.coroutines.*
-fun main(args: Array<String>) = runBlocking {
-    println("Here we go")
-    i = 0
-    launch { breakingFun()  }
-    for(x in 0..10) {
-        println("for in main $x")
+fun main(args: Array<String>) = runBlocking{
+    launch { createCoroutines() }
+    println()
+    println("------")
+    println()
+    for(x in 1..10){
+        print("$x-x ")
     }
-    launch { getBoolean() }
-    println("end main")
-}
-suspend fun someFun(){
-   println("start someFun")
-    for (x in 0..10){
-        println("$x in someFun")
-        delay(10)
-    }
-    print("-end someFun-")
-}
-fun breakingFun() = runBlocking {
-    println("starting breakingFun")
-    val jobs = List(10){
-        launch{
-            delay(1)
-            i += 1
-            someFun()
-            print("№$it-coroutine")
-        }
-    }
-    println("--------------------------")
-    jobs.forEach{ it.join()}
-    println("starting myJobs2")
-    val myJobs2 = List(20) {
-        launch {
-            delay(10)
-            print(" $it in jobs2")
-        }
-    }
-    myJobs2.forEach { it.join() }
-}
-suspend fun returnTrue(): Boolean {
-    delay(1000)
-    return true
-}
-suspend fun returnFalse(): Boolean{
-    delay(1000)
-    return false
-}
-fun getBoolean() = runBlocking {
-    val jobs3 = List(3) {
-        launch {
-            println("${returnFalse()} ${returnTrue()}")
-        }
-    }
-    println("in end of getBoolean()")
 }
 
-
-var i = 0
-
+fun createCoroutines() = runBlocking{
+    launch {
+        secondCoroutines()
+    }
+    launch {
+        delay(5000)
+        println()
+        println("------")
+        println()
+        for (z in 1..10) {
+            print("$z-z ")
+        }
+    }
+}
+suspend fun secondCoroutines(){
+    delay(4000)
+    println()
+    println("------")
+    println()
+    for(y in 1..10){
+        print("$y-y ")
+    }
+}
